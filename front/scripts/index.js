5e254f7007bcd18c498d8f5e67c448d75fa01bd0
kyohei8 <tsukuda.kyouhei@gmail.com>
2017-07-15 18:58:53 +0900

    update

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -10,16 +10,6 @@ import './tags/login.tag';
 import firebase from './firebase'
 import web3c from './modules/web3c'
 
-const url = 'http://localhost:8545';
-let web3;
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
-  // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider(url));
-}
-
-riot.mixin({web3});
 riot.mixin({web3c});
 riot.mixin(firebase);
 riot.mount('*');
--- front/scripts/modules/web3c.js
+++ front/scripts/modules/web3c.js
@@ -1,3 +1,16 @@
+import Web3 from 'Web3';
+import ethers from 'ethers';
+import request from 'superagent';
+
+const url = 'http://localhost:8545';
+let web3;
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider(url));
+}
+
 const CardMasterABI = JSON.parse(require('../../../sol/dist/CardMaster.abi'));
 const CardMasterBIN = `0x${require('../../../sol/dist/CardMaster.bin')}`;
 const CardABI = JSON.parse(require('../../../sol/dist/Card.abi'));
@@ -10,8 +23,8 @@ const CardMasterInstance = CardMasterContract.at(CardMasterAddress);
 
 const CardContract = web3.eth.contract(CardABI);
 
-
 const web3c = {
+  web3,
   // アンロック
   unlock: (userName, password, cb = () => {}) => {
     const unlockDurationSec = 0; // 0の場合永続的
